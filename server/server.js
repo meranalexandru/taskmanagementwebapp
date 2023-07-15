541a42107af142c892b2793af2a51b605d611bf5
meranalexandru <meraneugen20@stud.ase.ro>
2023-07-15 17:51:45 +0300

    finish task management app

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 require('dotenv').config()
 app.use(express.json())
